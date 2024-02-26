97f1fb23d2142ddc0921cfef9fc885fa5bf17c64
Ben Huang <120871839+benhuangbmj@users.noreply.github.com>
2024-02-26 20:16:06 +0000

    feat: linear regression

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,2,3])
+y = np.array([3.25,4,4.5])
+plt.plot(x,y, 'o')
+A = np.array([
+  [1,1],
+  [1,2],
+  [1,3]
+])
+ATran = np.transpose(A)
+ATA = np.dot(ATran, A)
+ATAInv = np.linalg.inv(ATA)
+ALeftInv = np.dot(ATAInv, ATran)
+beta = np.dot(ALeftInv, np.transpose(y))
+def line(x):
+  return beta[0] + beta[1]*x
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [1, 4, 2, 5])
-plt.ylabel('some numbers')
+plt.plot([1, 3], [line(1), line(3)])
 plt.show()
