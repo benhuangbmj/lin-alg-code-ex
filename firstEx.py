580ad7faebbe8e32ae37e04c284d9f949be4895d
Ben Huang <120871839+benhuangbmj@users.noreply.github.com>
2024-03-04 03:02:44 +0000

    feat: create firstEx.py

---
+++ firstEx.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def firstExample(x,y):
+  W = [];
+  for i in range(len(x)):
+    W.append([1, x[i]])
+
+  X = np.array(W)
+
+  XT = np.transpose(X)
+  XTX = np.matmul(XT, X)
+  XTXinv = np.linalg.inv(XTX)
+  beta = np.matmul((np.matmul(XTXinv, XT)), np.transpose(y));
+
+  def f(x):
+    return beta[0] + beta[1] * x
+
+  [a,b] = [min(x) - 1, max(x) + 1]
+  plt.plot([a,b], [f(a), f(b)])
+  plt.plot(x,y, 'o')
+  plt.show()
--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from firstEx import *
 x = np.array([1,2,3])
 y = np.array([3.25,4,4.5])
-plt.plot(x,y, 'o')
-A = np.array([
-  [1,1],
-  [1,2],
-  [1,3]
-])
-ATran = np.transpose(A)
-ATA = np.dot(ATran, A)
-ATAInv = np.linalg.inv(ATA)
-ALeftInv = np.dot(ATAInv, ATran)
-beta = np.dot(ALeftInv, np.transpose(y))
-def line(x):
-  return beta[0] + beta[1]*x
 
-plt.plot([1, 3], [line(1), line(3)])
-plt.show()
+firstExample(x,y)
