5b7a742c5ec854c276625a496e467a60944524c3
Ben Huang <120871839+benhuangbmj@users.noreply.github.com>
2024-03-06 16:47:25 +0000

    refactor: improve the visualization

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from secondEx import *
 
-secondEx('X3')
+'''data = pd.read_csv('realEstateAbridged.csv',sep=',')
+col = list(data.columns)
+
+X = np.array(pd.DataFrame(data, columns=[col[3]]))
+Y = np.array(pd.DataFrame(data, columns=[col[7]]))
+
+X = np.insert(X, 0, 1, axis=1)'''
--- secondEx.py
+++ secondEx.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def secondEx(xVar='X4'):
-# define a one-row-two-column subplot to display two figures side by side
+def secondEx(xVar='X2'):
+# define a one-row-two-column plot to display two figures side by side. The subplots are stored in axs
   fig, axs = plt.subplots(1,2)
-# load the data
-  data = pd.read_csv('realEstateAbridged.csv',sep=',')
+# load the data and take the first column as the index
+  data = pd.read_csv('realEstateAbridged.csv',index_col=0, sep=',')
 # capture the headers of the table
-  col = list(data.columns)
-# remove the "No" header 
-  col.remove('No')
-# plot a scatter plot and store it in the first figure
-  data.plot(x=xVar,y='Y', marker='.', linestyle='none', ax=axs[0])
+  col = data.columns.tolist()
+# get the full headers from realEstate.csv
+  headers = pd.read_csv('realEstate.csv', index_col=0, nrows=0).columns.tolist();
+# plot a scatter plot and store it in the first figure. Use the full header for as labels
+  data.plot(x=xVar, xlabel=headers[col.index(xVar)], y='Y', ylabel=headers[col.index('Y')], marker='.', linestyle='none', ax=axs[0])
 # create a dataFrame with the selected columuns
   dataFrame = pd.DataFrame(data, columns=col)
 # find the correlation matrix
