2dac2433954fd4cbef324c839bed62663767cf5e
Ben Huang <120871839+benhuangbmj@users.noreply.github.com>
2024-03-07 02:32:13 +0000

    feat: build linear regression

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-'''data = pd.read_csv('realEstateAbridged.csv',sep=',')
-col = list(data.columns)
+data = pd.read_csv('realEstateAbridged.csv',sep=',', index_col=0)
+headers = data.columns.tolist()
+xVar = headers[2]
 
-X = np.array(pd.DataFrame(data, columns=[col[3]]))
-Y = np.array(pd.DataFrame(data, columns=[col[7]]))
+X1 = np.array(pd.DataFrame(data, columns=headers[2:3]))
+Y = np.array(pd.DataFrame(data, columns=[headers[6]]))
 
-X = np.insert(X, 0, 1, axis=1)'''
+X = np.insert(arr=X1, obj=0, values=1, axis=1)
+beta = np.linalg.lstsq(X, Y, rcond=None)[0]
+def f(x):
+  return beta[0] + beta[1]*x
+fig, ax = plt.subplots()
+data.plot(x=xVar, xlabel=xVar, y=headers[6], ylabel=headers[6], marker='.', linestyle='none', ax=ax, color='y')
+ax.plot([min(X1), max(X1)], [f(min(X1)), f(max(X1))])
+plt.show()
